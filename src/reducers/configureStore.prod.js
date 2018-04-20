24e45f2ddf9401dd45aca42313bd407e189c24c3
Thomas Ricci <grisou13@hotmail.fr>
2018-04-20 11:47:25 +0200

    update

--- src/reducers/configureStore.prod.js
+++ src/reducers/configureStore.prod.js
@@ -1,18 +1,36 @@
-import { createStore,combineReducers, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore,combineReducers, compose } from 'redux'
 import { reducer as ui } from 'redux-ui'
 import DevTools from './../containers/DevTools';
+// import {routerReducer, routerMiddleware } from 'react-router-redux'
 
 import errors from './error'
+import taxForm from './taxForm'
+import taxCalculation from './taxCalculation'
+import taxSpending from './taxSpending'
 import middlewares from './middlewares'
+import { APP_READY } from '../consts/app';
 
+const app = (state = {ready:false}, {type, payload, ...meta}) => {
+  switch(type){
+    case APP_READY:
+      return {...state, ready:true}
+    default:
+      return state
+  }
+}
 const reducers = combineReducers({
     ui,
-    errors
+    errors,
+    app,
+    form: taxForm,
+    calculation: taxCalculation,
+    spendings: taxSpending
 })
+
 const createEnhancers = () => compose(
+      // Middleware you want to use in development:
       middlewares,
-  );
+)
 
 export default (initialState) => {
   // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
@@ -21,5 +39,10 @@ export default (initialState) => {
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
 
+    // if (module.hot) {
+    //   module.hot.accept('./reducers', () =>
+    //     store.replaceReducer(reducers)
+    //   );
+    // }
   return store;
 }
